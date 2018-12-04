5144f2b51b68e6483b8dc9aa4cbbd8768a19f663
Carl Ljung <carlju@kth.se>
2018-12-04 14:06:27 +0100

    change ram imu to solution

--- include/uavcan_ros_bridge/uav_to_ros/imu.h
+++ include/uavcan_ros_bridge/uav_to_ros/imu.h
@@ -1,14 +1,14 @@
 #ifndef UAV_TO_ROS_IMU_H
 #define UAV_TO_ROS_IMU_H
 
-#include <uavcan/equipment/ahrs/RawIMU.hpp>
+#include <uavcan/equipment/ahrs/Solution.hpp>
 #include <uavcan_ros_bridge/uavcan_ros_bridge.h>
 #include <sensor_msgs/Imu.h>
 
 namespace uav_to_ros {
 
 template <>
-bool convert(const uavcan::equipment::ahrs::RawIMU& uav_msg, sensor_msgs::Imu& ros_msg);
+bool convert(const uavcan::equipment::ahrs::Solution& uav_msg, sensor_msgs::Imu& ros_msg);
 
 }
 
--- src/uav_to_ros/imu.cpp
+++ src/uav_to_ros/imu.cpp
@@ -3,15 +3,19 @@
 namespace uav_to_ros {
 
 template <>
-bool convert(const uavcan::equipment::ahrs::RawIMU& uav_msg, sensor_msgs::Imu& ros_msg)
+bool convert(const uavcan::equipment::ahrs::Solution& uav_msg, sensor_msgs::Imu& ros_msg)
 {
     ros_msg.header.stamp = convert_timestamp(uav_msg.timestamp);
-    ros_msg.linear_acceleration.x = uav_msg.accelerometer_latest[0];
-    ros_msg.linear_acceleration.y = uav_msg.accelerometer_latest[1];
-    ros_msg.linear_acceleration.z = uav_msg.accelerometer_latest[2];
-    ros_msg.angular_velocity.x = uav_msg.rate_gyro_latest[0];
-    ros_msg.angular_velocity.y = uav_msg.rate_gyro_latest[1];
-    ros_msg.angular_velocity.z = uav_msg.rate_gyro_latest[2];
+    ros_msg.orientation.x = uav_msg.orientation_xyzw[0];
+    ros_msg.orientation.y = uav_msg.orientation_xyzw[1];
+    ros_msg.orientation.z = uav_msg.orientation_xyzw[2];
+    ros_msg.orientation.w = uav_msg.orientation_xyzw[3];
+    ros_msg.linear_acceleration.x = uav_msg.linear_acceleration[0];
+    ros_msg.linear_acceleration.y = uav_msg.linear_acceleration[1];
+    ros_msg.linear_acceleration.z = uav_msg.linear_acceleration[2];
+    ros_msg.angular_velocity.x = uav_msg.angular_velocity[0];
+    ros_msg.angular_velocity.y = uav_msg.angular_velocity[1];
+    ros_msg.angular_velocity.z = uav_msg.angular_velocity[2];
     return true;
 }
 
