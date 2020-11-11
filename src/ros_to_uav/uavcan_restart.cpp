a56569b092dc4c80cf7e4cf81922af0e28c13704
Carl Ljung <carlju@kth.se>
2020-11-11 09:18:49 +0100

    add restart service

---
+++ src/ros_to_uav/uavcan_restart.cpp
@@ -0,0 +1,20 @@
+#include <uavcan_ros_bridge/ros_to_uav/uavcan_restart.h>
+
+namespace ros_to_uav {
+
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+template <>
+bool convert_request(const uavcan_ros_bridge::UavcanRestartNode::Request& ros_request, uavcan::protocol::RestartNode::Request& uav_request)
+{
+    uav_request.magic_number = uav_request.MAGIC_NUMBER;
+    return true;
+}
+
+template <>
+bool convert_response(const uavcan::protocol::RestartNode::Response& uav_response, uavcan_ros_bridge::UavcanRestartNode::Response& ros_response)
+{
+    return true;
+}
+#pragma GCC diagnostic pop
+
+}
